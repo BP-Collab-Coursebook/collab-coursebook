d0f1d891356b2e4ec4936a33a6046a298564da8e
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-03-08 19:48:58 +0100

    Update

--- test/frontend/forms/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Purpose of this file
-
-Marks this directory as Python package directories. This package contains
-frontend/forms module related test cases.
-"""
--- test/frontend/templatetags/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Purpose of this file
-
-Marks this directory as Python package directories. This package contains
-frontend/templatetags module related test cases.
-"""
