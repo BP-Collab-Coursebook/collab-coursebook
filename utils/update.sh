7ea2077c21d901810025435762eb821f8eb971c9
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-03-09 18:48:09 +0100

    Update update.sh

--- utils/update.sh
+++ utils/update.sh
@@ -19,6 +19,7 @@ git pull
 pip install --upgrade setuptools pip wheel
 pip install --upgrade -r requirements.txt
 
+./manage.py makemigrations
 ./manage.py migrate
 ./manage.py collectstatic --noinput
 ./manage.py compilemessages
